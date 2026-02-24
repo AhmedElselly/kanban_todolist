462dff1d0c382ac24f41f1fd13ae5567861912da
ahmed elselly <ahmedelselly87@gmail.com>
2026-02-24 18:02:48 +0200

    added query call back after update single todo

--- src/hooks/updateTodo.ts
+++ src/hooks/updateTodo.ts
@@ -1,9 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateTodo } from "../api/todos";
 
 const useUpdateTodo = () => {
+  const queryClient = useQueryClient();
   const result = useMutation({
     mutationFn: updateTodo,
+    onSuccess: () => {
+      // 🔥 refetch todos
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
   });
 
   return result;
